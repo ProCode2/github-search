ca914a839d555bc64455f19ce051bb48012e42f0
Adibe Mohamed <52131981+adibemohamed@users.noreply.github.com>
2021-10-08 07:09:44 +0100

    feat: add search & dropdown for languages (#11)

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,20 @@
 <script>
+  import langsJSON from "./languages.json";
+
   let labels = "hacktoberfest";
   let lans = "";
+
   let repos = "";
   let orgs = "";
   let baseUrl =
     "https://github.com/issues?q=is:issue is:open sort:created-desc";
   let searchUrl = "";
 
-  let isAssigned = true
+  let isAssigned = true;
+
+  let showLangsDropdown = false;
+  let langsList = langsJSON;
+  let part = "";
 
   $: {
     const labelTags = labels
@@ -15,34 +22,78 @@
       .filter((tag) => tag)
       .map((l) => `label:"${l.trim()}"`)
       .join(" ");
+
     const lanTags = lans
       .split(",")
       .filter((tag) => tag)
       .map((l) => `language:${l.trim()}`)
       .join(" ");
+
     const repoTags = repos
       .split(",")
       .filter((tag) => tag)
       .map((r) => `repo:${r.trim()}`)
       .join(" ");
+
     const orgTags = orgs
       .split(",")
       .filter((tag) => tag)
       .map((o) => `org:${o.trim()}`)
       .join(" ");
 
-    let noAssignee = " "
-    if(isAssigned)
-      noAssignee = " no:assignee "
+    let noAssignee = " ";
+    if (isAssigned) noAssignee = " no:assignee ";
 
-    searchUrl = `${baseUrl}${noAssignee}${labelTags && labelTags} ${lanTags && lanTags} ${
-      repoTags && repoTags
-    } ${orgTags && orgTags}`;
+    searchUrl = `${baseUrl}${noAssignee}${labelTags && labelTags} ${
+      lanTags && lanTags
+    } ${repoTags && repoTags} ${orgTags && orgTags}`;
   }
 
-  const onKeyPress = e => {
-    if (e.charCode === 13) window.open(searchUrl, '_blank').focus();
-  }
+  const onKeyPress = (e) => {
+    if (e.charCode === 13) window.open(searchUrl, "_blank").focus();
+  };
+
+  // remove or add language
+  const handleLang = (lang) => {
+    let index = langsList.findIndex((x) => x.name == lang);
+    if (!langsList[index].selected) {
+      lans += lans.length > 0 ? `,${lang}` : `${lang}`;
+      langsList[index].selected = true;
+    } else {
+      langsList[index].selected = false;
+
+      // remove item from lans string
+      let lansArray = lans.split(",");
+      let itemToRemoveIndex = lansArray.indexOf(lang);
+      lansArray.splice(itemToRemoveIndex, 1);
+      lans = lansArray.join(",");
+    }
+    part = "";
+    langsList = langsJSON;
+  };
+
+  const searchLang = () => {
+    langsList = langsJSON.filter(
+      (item) => item.name.toLowerCase().substring(0, part.length) === part
+    );
+  };
+
+  const appendAllLangs = () => {
+    lans = "";
+    langsList.forEach((lang) => {
+      lang.selected = true;
+      lans += lans.length > 0 ? `,${lang.name}` : `${lang.name}`;
+    });
+    langsList[0].selected = true;
+  };
+
+  const removeAllLangs = () => {
+    lans = "";
+    langsList.forEach((lang) => {
+      lang.selected = false;
+    });
+    langsList[0].selected = false;
+  };
 </script>
 
 <main>
@@ -66,12 +117,55 @@
       </div>
       <div class="form-input">
         <label for="language">Languages</label>
-        <input
-          bind:value={lans}
-          name="language"
-          type="text"
-          placeholder="comma separated language."
-        />
+        <div>
+          <input
+            bind:value={lans}
+            on:click={() => {
+              showLangsDropdown = true;
+            }}
+            name="language"
+            type="text"
+            placeholder="comma separated language."
+            autoComplete="off"
+          />
+
+          {#if showLangsDropdown}
+            <div class="form-dropdown">
+              <input
+                bind:value={part}
+                on:input={searchLang}
+                type="text"
+                placeholder="Search languages"
+                autoComplete="off"
+              />
+              <div class="buttons-group">
+                <button
+                  on:click={() => {
+                    appendAllLangs();
+                  }}>select all</button
+                >
+                <button
+                  on:click={() => {
+                    removeAllLangs();
+                  }}>remove all</button
+                >
+              </div>
+              <div class="dropdown-items">
+                {#each langsList as lang}
+                  <div
+                    on:click={() => handleLang(lang.name)}
+                    class="dropdown-item"
+                  >
+                    <span>{lang.name}</span>
+                    {#if lang.selected}
+                      <img src="./checked.png" alt="" />
+                    {/if}
+                  </div>
+                {/each}
+              </div>
+            </div>
+          {/if}
+        </div>
       </div>
       <div class="form-input">
         <label for="repo">Repositories</label>
@@ -93,17 +187,22 @@
       </div>
       <div class="form-input">
         <label for="no-assignee">Not assigned to anyone</label>
-        <input
-          name="no-assignee"
-          type="checkbox"
-          bind:checked={isAssigned}
-        />
+        <input name="no-assignee" type="checkbox" bind:checked={isAssigned} />
       </div>
       <a class="form-button" href={searchUrl} target="_blank">
         View recent issues in Github
       </a>
     </div>
   </section>
+
+  {#if showLangsDropdown}
+    <div
+      id="dimmer"
+      on:click={() => {
+        showLangsDropdown = false;
+      }}
+    />
+  {/if}
 </main>
 
 <style>
@@ -154,17 +253,20 @@
   .form-input input {
     padding: 10px;
     border: 0.5px solid rgba(43, 53, 49, 1);
-    border-radius: 7px;
+    border-radius: 4px;
     width: 200px;
   }
 
   .form-button {
-    background-color: rgba(43, 53, 49, 1);
+    background-color: rgba(43, 53, 49, 0.9);
     color: white;
-    padding: 8px 10px;
+    padding: 15px 10px;
     border-radius: 4px;
     border: 0;
     margin: 20px 10px;
+  }
+  .form-button:hover {
+    background-color: rgba(43, 53, 49, 1);
   }
 
   .form-logo {
@@ -174,5 +276,78 @@
   a {
     text-decoration: none;
     font-size: 17px;
+  }
+
+  input {
+    font-size: 15px;
+  }
+
+  input:focus {
+    box-shadow: 0 0 0 0.125em #f747004d;
+    border: 1px solid #f74700 !important;
+    outline: none;
+  }
+
+  .form-dropdown {
+    position: absolute;
+    background: #fff;
+    margin-top: 2px;
+    border: 1px solid lightgray;
+    padding: 5px;
+    width: 220px;
+    z-index: 101;
+  }
+
+  .form-dropdown input {
+    margin-bottom: 5px;
+    padding: 10px;
+    border: 0.5px solid rgba(43, 53, 49, 1);
+    border-radius: 4px;
+    width: initial !important;
+  }
+
+  .buttons-group {
+    display: flex;
+  }
+
+  .buttons-group button {
+    flex: 1;
+    padding: 10px;
+    cursor: pointer;
+    border: 0;
+    font-size: 15px;
+    background-color: rgb(235, 235, 235);
+  }
+
+  .buttons-group button:hover {
+    background-color: rgb(218, 218, 218);
+    transition-delay: 0.2s;
+  }
+
+  .dropdown-items {
+    max-height: 333px;
+    overflow-y: auto;
+    margin-top: 5px;
+  }
+
+  .dropdown-item {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 5px;
+    border-bottom: 1px solid lightgray;
+    cursor: pointer;
+  }
+
+  .dropdown-item img {
+    height: 15px;
+  }
+
+  #dimmer {
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 100;
   }
 </style>
